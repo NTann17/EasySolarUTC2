94345b11fc33a08855806f9189e8fae30b0f8304
LUU TOAN DINH <dinhlt@ueh.edu.vn>
2025-10-11 15:58:50 +0700

    update admin layout

---
+++ SolarK64/Controllers/AccountController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SolarK64.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("/admin")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SolarK64/Controllers/AdminController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SolarK64.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Route("/admin/dashboard")]
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
